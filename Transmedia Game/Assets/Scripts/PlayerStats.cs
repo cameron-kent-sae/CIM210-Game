3c5b3622fb326a9f59b579ab0c9a7d7e960e7796
ConnorYounger <72112139+ConnorYounger@users.noreply.github.com>
2021-04-14 15:51:35 +1000

    PlayerStats Scriptable object

---
+++ Transmedia Game/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player Stats", menuName = "Character Stats/Player Stats")]
+
+public class PlayerStats : ScriptableObject
+{
+    public string characterName = "James";
+
+    public Sprite playerSprite;
+
+    public float influenceLevel = 1;
+    public float conditionLevel = 40;
+}
