958b50ec919b082df4858d03811111c751106be0
ConnorYounger <72112139+ConnorYounger@users.noreply.github.com>
2021-04-14 16:07:25 +1000

    ScenarioManager

---
+++ Transmedia Game/Assets/Scripts/AIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIManager : MonoBehaviour
+{
+    public AIStats[] aiCharacters;
+
+    public void UpdateAiStats(float influence, float condition)
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Transmedia Game/Assets/Scripts/ScenarioManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioManager : MonoBehaviour
+{
+    public List<Scenario> scenarios;
+
+    public PlayerStats playerStats;
+
+    public AIManager aiManager;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ButtonSelected(float influence, float condition)
+    {
+        if (aiManager)
+        {
+            aiManager.UpdateAiStats(influence, condition);
+        }
+        else
+        {
+            Debug.LogError("Missing aiManager");
+        }
+
+
+    }
+
+    void LoadNextScenario()
+    {
+        if(scenarios.Count > 0)
+        {
+            // Do Stuff
+        }
+        else
+        {
+            Debug.LogError("No scenarios in the list");
+        }
+    }
+}
