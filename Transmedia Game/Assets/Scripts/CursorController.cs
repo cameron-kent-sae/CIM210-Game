bbb508eca0c944f2f4a6b699fa0b10f82efd157e
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-14 13:37:25 +1000

    Update Cursor

---
+++ Transmedia Game/Assets/Scripts/CursorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+
+    private Vector2 hotSpot = Vector2.zero;
+    private CursorMode cursorMode = CursorMode.Auto;
+
+    /*
+    private void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }*/
+
+    void OnMouseOver()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+}
