d2be5b73e0567a48bdc26d4a641d6df8552635f0
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-21 22:54:57 +1000

    Update Main

--- Transmedia Game/Assets/Scripts/MenuController.cs
+++ Transmedia Game/Assets/Scripts/MenuController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -6,12 +8,28 @@ public class MenuController : MonoBehaviour
 {
     public Button playBtn, creditsBtn, exitBtn;
     public string gameScene, creditsScene;
+    public CanvasGroup canvasGroup;
+    public float fadeLength;
 
     public void Start()
     {
         playBtn.onClick.AddListener(PlayGame);
         creditsBtn.onClick.AddListener(ShowCredits);
         exitBtn.onClick.AddListener(ExitGame);
+
+        StartCoroutine("FadeInCanvas");
+    }
+
+
+     IEnumerator FadeInCanvas()
+    {
+        yield return new WaitForSeconds(1);
+
+        for (float t = 0f; t < fadeLength; t += Time.deltaTime)
+        {
+            canvasGroup.alpha = t;
+            yield return null;
+        }
     }
 
     private void PlayGame()
