ce3ed25cf23241327938f6ed220e059754f00a0f
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-26 11:15:06 +1000

    Update game background music

--- Transmedia Game/Assets/Scripts/AudioManager.cs
+++ Transmedia Game/Assets/Scripts/AudioManager.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip[] audioClips;
+    private AudioSource audioSource;
+
+    private void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        PlayRandomAudio();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayRandomAudio()
     {
-        
+        audioSource.clip = audioClips[UnityEngine.Random.Range(1, audioClips.Length)];
+        audioSource.Play();
     }
 }
