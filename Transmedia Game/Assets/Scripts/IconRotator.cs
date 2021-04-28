624e186dc599cbf8851a2856859aa1c71f813292
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-28 21:39:25 +1000

    Created Counting frame

---
+++ Transmedia Game/Assets/Scripts/IconRotator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconRotator : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(RotateObject(90, Vector3.back, 1));
+    }
+
+    IEnumerator RotateObject(float angle, Vector3 axis, float inTime)
+    {
+        // calculate rotation speed
+        float rotationSpeed = angle / inTime;
+
+        while (true)
+        {
+            // save starting rotation position
+            Quaternion startRotation = transform.rotation;
+
+            float deltaAngle = 0;
+
+            // rotate until reaching angle
+            while (deltaAngle < angle)
+            {
+                deltaAngle += rotationSpeed * Time.deltaTime;
+                deltaAngle = Mathf.Min(deltaAngle, angle);
+
+                transform.rotation = startRotation * Quaternion.AngleAxis(deltaAngle, axis);
+
+                yield return null;
+            }
+
+            // delay here
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
